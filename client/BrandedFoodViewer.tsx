cadde6061d4f9a2eab393702bf6cc65829526f79
Kester Tong <kestert@google.com>
2020-02-21 08:24:09 -0500

    Add Edit functions to branded food

--- client/BrandedFoodViewer.tsx
+++ client/BrandedFoodViewer.tsx
@@ -21,41 +21,69 @@ import { NormalizedFood } from "../core/NormalizedFood";
 import { NutrientInfo } from "../core/Nutrients";
 import { NutrientsViewer } from "./NutrientsViewer";
 
-export const BrandedFoodViewer: React.SFC<{food: BrandedFood, normalizedFood: NormalizedFood, nutrientInfos: NutrientInfo[], editMode: boolean}> = (props) => {
-  let food = props.food;
-  if (props.editMode) {
-    return <React.Fragment>
-      <h1>{food.description} <em>{food.brandOwner ? '(' + food.brandOwner + ')' : ''}</em></h1>
-      <Form>
-        <Form.Row>
-          <Form.Group as={Col} controlId="formHouseholdUnit">
-            <Form.Label>Household Units</Form.Label>
-            <Form.Control value={food.householdServingFullText!} />
+interface BrandedFoodViewerProps {
+  food: BrandedFood;
+  normalizedFood: NormalizedFood;
+  nutrientInfos: NutrientInfo[];
+  editMode: boolean;
+}
+
+export class BrandedFoodViewer extends React.Component<BrandedFoodViewerProps, BrandedFood> {
+
+  state = { ...this.props.food };
+
+  render() {
+    let food = this.state;
+    if (this.props.editMode) {
+      return <React.Fragment>
+        <h1>{food.description} <em>{food.brandOwner ? '(' + food.brandOwner + ')' : ''}</em></h1>
+        <Form>
+          <Form.Group controlId="formDescription">
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                value={food.description}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {this.setState({description: event.target.value})}}
+                />
           </Form.Group>
-          <Form.Group as={Col} controlId="formServingSize">
-            <Form.Label>Serving Size</Form.Label>
-            <Form.Control type="number" value={food.servingSize.toString()}/>
-          </Form.Group>
-          <Form.Group as={Col} controlId="formServingSizeUnits">
-            <Form.Label>Units</Form.Label>
-            <Form.Control as="select" value={food.servingSizeUnit}>
-              <option>ml</option>
-              <option>g</option>
-            </Form.Control>
-          </Form.Group>
-        </Form.Row>
-      </Form>
-    </React.Fragment>;
-  } else {
-    return <React.Fragment>
-      <h1>{food.description} <em>{food.brandOwner ? '(' + food.brandOwner + ')' : ''}</em></h1>
-      { food.ingredients ? <React.Fragment><h2>Ingredients</h2>{food.ingredients}</React.Fragment> : null }
-      <h2>Nutrients</h2>
-      <NutrientsViewer 
-          food={food}
-          normalizedFood={props.normalizedFood}
-          nutrientInfos={props.nutrientInfos}
-          key={food.description} />
-    </React.Fragment>;
+          <Form.Row>
+            <Form.Group as={Col} controlId="formHouseholdUnit">
+              <Form.Label>Household Units</Form.Label>
+              <Form.Control
+                value={food.householdServingFullText!}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {this.setState({householdServingFullText: event.target.value})}}
+                />
+            </Form.Group>
+            <Form.Group as={Col} controlId="formServingSize">
+              <Form.Label>Serving Size</Form.Label>
+              <Form.Control
+                type="number"
+                value={food.servingSize.toString()}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {this.setState({servingSize: Number(event.target.value)})}}
+                />
+            </Form.Group>
+            <Form.Group as={Col} controlId="formServingSizeUnits">
+              <Form.Label>Units</Form.Label>
+              <Form.Control
+                as="select" value={food.servingSizeUnit}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {this.setState({servingSizeUnit: event.target.value})}}>
+                <option>ml</option>
+                <option>g</option>
+              </Form.Control>
+            </Form.Group>
+          </Form.Row>
+        </Form>
+      </React.Fragment>;
+    } else {
+      return <React.Fragment>
+        <h1>{food.description} <em>{food.brandOwner ? '(' + food.brandOwner + ')' : ''}</em></h1>
+        { food.ingredients ? <React.Fragment><h2>Ingredients</h2>{food.ingredients}</React.Fragment> : null }
+        <h2>Nutrients</h2>
+        <NutrientsViewer 
+            food={food}
+            normalizedFood={this.props.normalizedFood}
+            nutrientInfos={this.props.nutrientInfos}
+            key={food.description} />
+      </React.Fragment>;
+    }
   }
 }
